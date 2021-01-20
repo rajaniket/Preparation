36889a439da7d417d9399ba299e2f32e0f1260ed
Aniket Raj <aniketraj.go@gmail.com>
2021-01-20 23:59:25 +0530

    Create slot_1.cpp

---
+++ TCS/slot_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n=10,k=5;
+    int num;
+    cin>>num;
+    if(num<=k && num>=1)
+    {
+        cout<<"NUMBER OF CANDIES SOLD : "<<num<<endl;
+        cout<<"NUMBER OF CANDIES AVAILABLE : "<<n-num<<endl;
+    }
+    else
+    {
+        cout<<"INVALID INPUT"<<endl;
+        cout<<"NUMBER OF CANDIES LEFT : "<<n-num<<endl;
+
+    }
+
+}
